deeb3d2d3533e55f442e7792f725c311715067a9
shinara03 <shinara03@gmail.com>
2021-02-22 11:37:13 -0800

    add user model and user validation

---
+++ validation/register.js
@@ -0,0 +1,61 @@
+const Validator = require('validator');
+const validText = require('./valid-text');
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+  data.firstName = validText(data.firstName) ? data.firstName : '';
+  data.lastName = validText(data.lastName) ? data.lastName : '';
+  data.phoneNumber = validText(data.phoneNumber) ? data.phoneNumber : '';
+  data.email = validText(data.email) ? data.email : '';
+  data.password = validText(data.password) ? data.password : '';
+  data.password2 = validText(data.password2) ? data.password2 : '';
+
+  // if (!Validator.isLength(data.handle, { min: 2, max: 30 })) {
+  //   errors.handle = 'Handle must be between 2 and 30 characters';
+  // }
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = 'First name is required';
+  }
+
+  if (Validator.isEmpty(data.lastName)) {
+    errors.lastName = 'Last name is required';
+  }
+
+  if (Validator.isEmpty(data.phoneNumber)) {
+    errors.phoneNumber = 'Phone Number is required';
+  }
+
+  if (!Validator.isLength(data.phoneNumber, { min: 10 })) {
+    errors.phoneNumber = 'Phone Number must be at least 10 characters';
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'Email field is required';
+  }
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid';
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be at least 6 characters';
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = 'Confirm Password field is required';
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = 'Passwords must match';
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
