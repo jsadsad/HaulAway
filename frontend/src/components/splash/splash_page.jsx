74d7749b088f46c526c405b53be92563ab24037e
kinda-dev <maybe.fabio@gmail.com>
2021-02-22 16:29:18 -0800

    style splashpage

--- frontend/src/components/splash/splash_page.jsx
+++ frontend/src/components/splash/splash_page.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import './splash.css'
 
 const SplashPage = () => {
     return (
         <div className="splash-page">
             
-                <header className="login-header">
-                    <div className="login-header-wrapper">
+                <header className="splash-header">
+                    <div className="splash-header-wrapper">
+
                         <div className="splash-signup-wrap">
-                            <button className="splash-signup-button">signup</button>
+                            <button className="splash-signup-button">SIGNUP</button>
                         </div>
                         <div className="splash-login-buttons-wrap">
-                            <button className="splash-demo-login">demo login</button>
-                            <button className="splash-login-button"></button>
+                            <div className="splash-demo-login-wrap"><button className="splash-demo-login">DEMO LOGIN</button></div>
+                            <div className="splash-login-wrap"><button className="splash-login-button">LOGIN</button></div>
                         </div>
-                    </div>
-                </header>
-                <div className="splash-body">
-                    <div className="splash-text-wrap">
-                        <div className="splash-haulaway-text-logo"></div>
 
                     </div>
+                </header>
+
+                <div className="splash-body">
+                    <img className="splash-background" src="https://haul-seeds.s3-us-west-1.amazonaws.com/ha-1.jpg" alt="background"/>
+                    <div className="splash-text-wrap">
+                        <h1 className="splash-haulaway-text-logo">HaulAway</h1>
+                        <h2 className="splash-slogan">Hauling Reinvented</h2>
+                        <h4 className="splash-little-motivation">Technology for Humans, by Humans</h4>
+                    </div>
+                    <div className="center-pixel"></div>
                 </div>
         </div>
     )
