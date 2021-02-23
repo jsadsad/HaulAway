f86b2353ae719a4713a2749a5296fb8a2173ace9
shinara03 <shinara03@gmail.com>
2021-02-22 21:47:15 -0800

    change app  and package.json

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const users = require('./routes/api/users')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const User = require('./models/User')
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -17,6 +25,7 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err))
+
 
 app.get('/', (req, res) => res.send('Hello World'))
 
