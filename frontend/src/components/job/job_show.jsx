03570a30d51ad6d5a4b6d9daf3c086cc35e89c44
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-25 05:16:43 +0600

    Create JobShow skele component

--- frontend/src/components/job/job_show.jsx
+++ frontend/src/components/job/job_show.jsx
@@ -1,17 +1,70 @@
-import React from 'react';
+import React from 'react'
 
 class JobShow extends React.Component {
-
   render() {
     return (
       <div className="job-show-outer">
         <div className="job-show-wrapper">
-          <div className="job-show-poster">
-            <h2>Job Poster</h2>
+          <div className="job-show-header">
+            <div className="job-show-poster">
+              <h2>Job Poster</h2>
+            </div>
+
+            <div className="job-show-box">
+              <input
+                type="text"
+                className="job-show-input-desc"
+                placeholder="Description"
+              />
+            </div>
+
+            <div className="job-show-box">
+              <input
+                type="text"
+                className="job-show-input-diff"
+                placeholder="Difficulty"
+              />
+            </div>
           </div>
-          
+          <div className="job-show-body">
+            <div className="job-show-box">
+              <input
+                type="date"
+                className="job-show-input-start"
+                placeholder="Start Date"
+              />
+            </div>
+            <div className="job-show-box">
+              <input
+                type="date"
+                className="job-show-input-end"
+                placeholder="End Date"
+              />
+            </div>
+            <div className="job-show-box">
+              <input
+                type="text"
+                className="job-show-input-pickup"
+                placeholder="Pickup"
+              />
+            </div>
+            <div className="job-show-box">
+              <input
+                type="text"
+                className="job-show-input-dest"
+                placeholder="Destination"
+              />
+            </div>
+          </div>
+          <div className="job-show-footer">
+            <div className="job-show-map">
+              <h2>Ill be a map</h2>
+            </div>
+          </div>
         </div>
       </div>
     )
   }
 }
+
+export default JobShow;
