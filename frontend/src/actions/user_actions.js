467275e629cdc2e73ec53372fc9c4a26f2a21494
shinara03 <shinara03@gmail.com>
2021-02-23 21:17:08 -0800

    user api util, actions, and reducer

---
+++ frontend/src/actions/user_actions.js
@@ -0,0 +1,26 @@
+import {getUser, getUsers} from '../util/user_api_util'
+
+export const RECEIVE_USERS = "RECEIVE_USERS"
+export const RECEIVE_USER = "RECEIVE_USER"
+
+export const receiveUsers = users => ({
+  type: RECEIVE_USERS, 
+  users
+})
+
+export const receiveUser = user => ({
+  type: RECEIVE_USER, 
+  user
+})
+
+export const fetchUsers = () => dispatch => (
+  getUsers()
+    .then(users => dispatch(receiveUsers(users)))
+    .catch(err => console.log(err))
+)
+
+export const fetchUser = id => dispatch => (
+  getUser(id)
+    .then(user => dispatch(receiveUser(user)))
+    .catch(err => console.log(err))
+)
