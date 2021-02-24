163d1587fde8f1f84dbdf5e5cad178c6037b9b9a
Dmitrii An <an.dmitriy93@gmail.com>
2021-02-24 18:56:50 +0600

    style job post finish

--- frontend/src/components/posts/job_post_form.jsx
+++ frontend/src/components/posts/job_post_form.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import Navbar from '../navbar/navbar'
 import './job_post.css'
 
@@ -10,22 +10,21 @@ class JobPostForm extends React.Component {
   render() {
     return (
       <div className="job-post-outer">
-
-          <Navbar />
+        <Navbar />
 
         <div className="job-post-container">
           <div className="job-post-form">
             <form onSubmit={this.handleSubmit} className="job-post-form-box">
-            <h2 className="job-post-text">Job Request</h2>
+              <h2 className="job-post-text">Job Request</h2>
               <div className="job-post-input-box">
                 <textarea
-                //   type="text"
+                  //   type="text"
                   className="job-post-input-desc"
                   placeholder="Description"
-                //   onChange={this.handleField('description')}
+                  //   onChange={this.handleField('description')}
                 />
               </div>
-              <br/>
+              <br />
               <div className="job-post-input-box">
                 <input
                   type="text"
@@ -34,55 +33,94 @@ class JobPostForm extends React.Component {
                 />
                 <input type="file" className="job-post-upload-btn" />
               </div>
-              <br/>
+              <br />
 
               <div className="job-post-lvl-btn">
                 <button>Easy</button>
                 <button>Medium</button>
                 <button>Hard</button>
               </div>
-              <br/>
+              <br />
 
               <div className="job-post-input-box">
-                  <input type="text" className="job-post-input-start" placeholder="start location" />
+                <input
+                  type="text"
+                  className="job-post-input-start"
+                  placeholder="start location"
+                />
               </div>
-              <br/>
+              <br />
 
               <div className="job-post-input-box">
-                  <input type="text" className="job-post-input-dest" placeholder="destination" />
+                <input
+                  type="text"
+                  className="job-post-input-dest"
+                  placeholder="destination"
+                />
               </div>
-              <br/>
+              <br />
 
               <div className="job-post-input-box">
-                  <input type="date" className="job-post-input-date" placeholder="start date" />
+                <input
+                  type="date"
+                  className="job-post-input-date"
+                  placeholder="start date"
+                />
               </div>
-              <br/>
+              <br />
               <div className="job-post-input-box">
-                  <input type="date" className="job-post-input-date2" placeholder="end date" />
+                <input
+                  type="date"
+                  className="job-post-input-date2"
+                  placeholder="end date"
+                />
               </div>
-              <br/>
+              <br />
 
               <button>Submit</button>
-
             </form>
           </div>
         </div>
         <div className="splash-footer">
-                    <div className="splash-footer-wrapper">
-                        {/* <div className="thank you-wrap"> */}
-                        <div className="thank-you">Thank you for your visit</div>
-                        {/* </div> */}
-                    <div className="splash-footer-info">
-                        <div className="engineerd-by">Engineerd with love by:</div>
-                        <div className="info-us">
-                        <a className="contact" href="https://github.com/shinara03" target="_blank">Lena</a>
-                        <a className="contact" href="https://github.com/andmitriy93" target="_blank">Dmitrii</a>
-                        <a className="contact" href="https://github.com/jsadsad" target="_blank">Josh</a>
-                        <a className="contact" href="https://github.com/kinda-dev" target="_blank">Fabio</a>
-                        </div>
-                    </div>
-                    </div>
-                </div>
+          <div className="splash-footer-wrapper">
+            {/* <div className="thank you-wrap"> */}
+            <div className="thank-you">Thank you for your visit</div>
+            {/* </div> */}
+            <div className="splash-footer-info">
+              <div className="engineerd-by">Engineerd with love by:</div>
+              <div className="info-us">
+                <a
+                  className="contact"
+                  href="https://github.com/shinara03"
+                  target="_blank"
+                >
+                  Lena
+                </a>
+                <a
+                  className="contact"
+                  href="https://github.com/andmitriy93"
+                  target="_blank"
+                >
+                  Dmitrii
+                </a>
+                <a
+                  className="contact"
+                  href="https://github.com/jsadsad"
+                  target="_blank"
+                >
+                  Josh
+                </a>
+                <a
+                  className="contact"
+                  href="https://github.com/kinda-dev"
+                  target="_blank"
+                >
+                  Fabio
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
