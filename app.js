0504b694102609862e5c3bdd9ecaf82c4b912535
JCS <>
2021-02-24 13:15:20 -0800

    user profile pictures uploading

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const db = require('./config/keys').mongoURI
@@ -7,12 +8,13 @@ const passport = require('passport')
 const users = require('./routes/api/users')
 const jobs = require('./routes/api/jobs')
 const User = require('./models/User')
-const path = require('path');
+const fileUploadRoutes = require('./routes/fileUploads')
+const path = require('path')
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('frontend/build'))
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
 }
 
@@ -27,11 +29,11 @@ mongoose
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.log(err))
 
-
 app.get('/', (req, res) => res.send('Hello World'))
 
 app.use('/api/users', users)
 app.use('/api/jobs', jobs)
+app.use('/api/photos', fileUploadRoutes)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server is running on port ${port}`))
---
+++ routes/fileUploads.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const router = express.Router()
+const PHOTO = require('../models/Photos')
+const multer = require('multer')
+let AWS = require('aws-sdk')
+
+require('dotenv').config()
+
+let storage = multer.memoryStorage()
+let upload = multer({ storage: storage })
+
+router.route('/').get((req, res, next) => {
+  PHOTO.find(
+    {},
+    null,
+    {
+      sort: { createdAt: 1 },
+    },
+    (err, docs) => {
+      if (err) {
+        return next(err)
+      }
+      res.status(200).send(docs)
+    }
+  )
+})
+
+router.route('/:id').get((req, res, next) => {
+  PHOTO.findById(req.params.id, (err, go) => {
+    if (err) {
+      return next(err)
+    }
+    res.json(go)
+  })
+})
+
+router.post('/upload', upload.single('file'), function (req, res) {
+  const file = req.file
+  const s3FileURL = process.env.AWS_Uploaded_File_URL_LINK
+
+  let s3bucket = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  })
+
+  let params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: file.originalname,
+    Body: file.buffer,
+    ContentType: file.mimetype,
+    ACL: 'public-read',
+  }
+
+  s3bucket.upload(params, function (err, data) {
+    if (err) {
+      res.status(500).json({ error: true, Message: err })
+    } else {
+      let newFileUploaded = {
+        description: req.body.description,
+        fileLink: s3FileURL + file.originalname,
+        s3_key: params.Key,
+      }
+      var photo = new PHOTO(newFileUploaded)
+      photo.save(function (error, newFile) {
+        if (error) {
+          throw error
+        }
+      })
+      let newData = Object.assign({}, data, { photoId: photo._id })
+      res.send({ newData })
+    }
+  })
+})
+
+router.route('/edit/:id').put((req, res, next) => {
+  PHOTO.findByIdAndUpdate(
+    req.params.id,
+    { $set: { description: Object.keys(req.body)[0] } },
+    { new: true },
+    (err, updateDoc) => {
+      if (err) {
+        return next(err)
+      }
+      res.status(200).send(updateDoc)
+    }
+  )
+})
+
+router.route('/:id').delete((req, res, next) => {
+  PHOTO.findByIdAndRemove(req.params.id, (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    let s3bucket = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    })
+
+    let params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: result.s3_key,
+    }
+
+    s3bucket.deleteObject(params, (err, data) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send({
+          status: '200',
+          responseType: 'string',
+          response: 'success',
+        })
+      }
+    })
+  })
+})
+
+module.exports = router
