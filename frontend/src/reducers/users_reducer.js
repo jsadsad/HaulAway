63b87fccc7109fc5cd7cdca61fc7b8ced90caa49
shinara03 <shinara03@gmail.com>
2021-02-23 21:19:17 -0800

    about to merge user-backend

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -4,7 +4,7 @@ const usersReducer = (state = {}, action) => {
   Object.freeze(state)
   switch(action.type) {
     case RECEIVE_USERS: 
-      return Object.assign({}, action.users.data)
+      return Object.assign({}, action.users.data) 
     case RECEIVE_USER:
       return Object.assign({}, {[action.user.data._id]: action.user.data})
     default: 
