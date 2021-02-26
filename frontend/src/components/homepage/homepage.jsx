f2b63f74f9af99148bcf7cf6fb3dcbb9e5376de4
shinara03 <shinara03@gmail.com>
2021-02-26 09:39:50 -0800

    finish homepage styling

--- frontend/src/components/homepage/homepage.jsx
+++ frontend/src/components/homepage/homepage.jsx
@@ -8,13 +8,16 @@ class Homepage extends React.Component {
   componentDidMount() {
     this.props.fetchJobs()
   }
+
   render() {
-    if(!this.props.jobs) {return null}
+    if(this.props.jobs.length === 0) {return null}
 
     let fourJobs = [];
     for(let i = 0; i < 4; i++) {
       fourJobs = fourJobs.concat(this.props.jobs[i])
     }
+
+    console.log(fourJobs)
     
     return (
       <div className="homepage-wrapper">
@@ -24,7 +27,7 @@ class Homepage extends React.Component {
               <img className='homepage-img' src='/box3.jpeg'/>
           </div>
           <div className="homepage-job-buttons">
-            {/* <div className='homepage-job-buttons-header'>Need help with your boxes?</div> */}
+            <div className='homepage-job-buttons-header'>Need help with your boxes?</div>
             <button
               onClick={() => this.props.history.push('/jobs/new')}
               className="homepage-rj-button"
@@ -35,18 +38,22 @@ class Homepage extends React.Component {
           </div>
         </div>
         <div className="homepage-job-index-wrapper">
-          <h2>Jobs</h2>
-          <div> 
-            {/* {fourJobs.map(job => {
+          <div className="homepage-job-index-header">
+            <h2>Looking for Jobs?</h2>
+            <h2 onClick={() => this.props.history.push('/jobs')}className='homepage-view-all'>View All</h2>
+          </div>
+          <div className="homepage-job-index"> 
+            {fourJobs.map(job => {
               return (
-                <div>
-                <div>Pickup: {job.pickup}</div>
-                <div>Destination: {job.destination}</div>
-                <div>Start Date: {job.jobStartDate}</div>
-                <div>End Date: {job.jobEndDate}</div>
+                <div key={job._id} className="homepage-job-index-items"
+                  onClick={() => this.props.history.push(`/job/${job._id}`)}>
+                  <div>Pickup: {job.pickup}</div>
+                  <div>Destination: {job.destination}</div>
+                  <div>Start Date: {job.jobStartDate}</div>
+                  <div>End Date: {job.jobEndDate}</div>
                 </div>
                 )
-              })} */}
+              })}
           </div>
         </div>
         {/* <div className="homepage-job-offer-wrapper"> */}
