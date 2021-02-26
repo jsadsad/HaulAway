32db28396a6aaadc28d62ff94dc4971c0107bcb3
JCS <>
2021-02-25 16:45:26 -0800

    fixed homepage create job button to right route

--- frontend/src/components/homepage/homepage.jsx
+++ frontend/src/components/homepage/homepage.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import Navbar from '../navbar/navbar_container';
-import './homepage.css';
-import { withRouter } from "react-router";
-
-
-
+import React from 'react'
+import Navbar from '../navbar/navbar_container'
+import './homepage.css'
+import { withRouter } from 'react-router'
 
 class Homepage extends React.Component {
-
   render() {
     // debugger
-    return(
-      <div className='homepage-wrapper'>
+    return (
+      <div className="homepage-wrapper">
         <Navbar />
-        <div className='homepage-job-index-wrapper'>
+        <div className="homepage-job-index-wrapper">
           <h2>Jobs</h2>
         </div>
-        <div className='homepage-job-button-wrapper'>
-          <div className='homepage-job-buttons'>
-            <button onClick={() => this.props.history.push('/job/new')} className='homepage-rj-button'>Request a Job</button>
-            <button className='homepage-oj-button'>Offer a Job</button>
+        <div className="homepage-job-button-wrapper">
+          <div className="homepage-job-buttons">
+            <button
+              onClick={() => this.props.history.push('/jobs/new')}
+              className="homepage-rj-button"
+            >
+              Request a Job
+            </button>
+            <button className="homepage-oj-button">Offer a Job</button>
           </div>
         </div>
-        <div className='homepage-job-offer-wrapper'>
+        <div className="homepage-job-offer-wrapper">
           <h2>Offers</h2>
         </div>
       </div>
