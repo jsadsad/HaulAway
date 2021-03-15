0a772ebf81c702abe626e903487e29afed5a80ab
shinara03 <shinara03@gmail.com>
2021-03-15 13:34:59 -0700

    finish login-page css

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -54,54 +54,97 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <div className="login-outer-wrap">
+      // <div className="login-outer-wrap">
 
-        <Navbar />
+      //   <Navbar />
             
-        <div className="login-body">
-          <div className="login-background"></div>  
-            <div className="login-form-container">
-              <h2 className="sign-in-letters">Sign In</h2>
-                <form onSubmit={this.handleSubmit} className="login-form-box">
-                  <br/>
-                  <div className="login-input-little-box">
-                    <input className="login-input-email" placeholder="Email" type="text" value={this.state.email} onChange={this.handleField('email')} />
-                    <div className="errors">{this.props.errors.email}</div>
-                  </div>
-                  <br/>
-                  <div className="login-input-little-box">
-                    <input className="login-input-password" placeholder="Password" type="password" value={this.state.password} onChange={this.handleField('password')}/>
-                    <div className="errors">{this.props.errors.password}</div>
-                  </div>
-                  <br/>
-                  <button className="login-button" >Sign In</button>
-                </form>
-                <br/>
-                <div className="sing-up-link-login-form">
-                  <p className="demo-user-wrap"><button onClick={this.loginDemo} className="demo-user-button">Demo User</button></p>
-                  <div className="sign-up-new">New to HaulAway? <Link className="sign-up-link" to="/signup">Sign up now</Link>.</div>
-                </div>
-            </div>
-          <div className="center-pixel"></div>
-        </div>
+      //   <div className="login-body">
+      //     <div className="login-background"></div>  
+      //       <div className="login-form-container">
+      //         <h2 className="sign-in-letters">Sign In</h2>
+      //           <form onSubmit={this.handleSubmit} className="login-form-box">
+      //             <br/>
+      //             <div className="login-input-little-box">
+      //               <input className="login-input-email" placeholder="Email" type="text" value={this.state.email} onChange={this.handleField('email')} />
+      //               <div className="errors">{this.props.errors.email}</div>
+      //             </div>
+      //             <br/>
+      //             <div className="login-input-little-box">
+      //               <input className="login-input-password" placeholder="Password" type="password" value={this.state.password} onChange={this.handleField('password')}/>
+      //               <div className="errors">{this.props.errors.password}</div>
+      //             </div>
+      //             <br/>
+      //             <button className="login-button" >Sign In</button>
+      //           </form>
+      //           <br/>
+      //           <div className="sing-up-link-login-form">
+      //             <p className="demo-user-wrap"><button onClick={this.loginDemo} className="demo-user-button">Demo User</button></p>
+      //             <div className="sign-up-new">New to HaulAway? <Link className="sign-up-link" to="/signup">Sign up now</Link>.</div>
+      //           </div>
+      //       </div>
+      //     <div className="center-pixel"></div>
+      //   </div>
 
-            <div className="splash-footer">
-              <div className="splash-footer-wrapper">
-                        {/* <div className="thank you-wrap"> */}
-                        <div className="thank-you">Thank you for your visit</div>
-                        {/* </div> */}
-                  <div className="splash-footer-info">
-                    <div className="engineerd-by">Engineered with love by:</div>
-                      <div className="info-us">
-                        <a className="contact" href="https://github.com/shinara03" target="_blank">Lena</a>
-                        <a className="contact" href="https://github.com/andmitriy93" target="_blank">Dmitrii</a>
-                        <a className="contact" href="https://github.com/jsadsad" target="_blank">Josh</a>
-                        <a className="contact" href="https://github.com/kinda-dev" target="_blank">Fabio</a>
-                      </div>
-                    </div>
+      //       <div className="splash-footer">
+      //         <div className="splash-footer-wrapper">
+ 
+      //                   <div className="thank-you">Thank you for your visit</div>
+
+      //             <div className="splash-footer-info">
+      //               <div className="engineerd-by">Engineered with love by:</div>
+      //                 <div className="info-us">
+      //                   <a className="contact" href="https://github.com/shinara03" target="_blank">Lena</a>
+      //                   <a className="contact" href="https://github.com/andmitriy93" target="_blank">Dmitrii</a>
+      //                   <a className="contact" href="https://github.com/jsadsad" target="_blank">Josh</a>
+      //                   <a className="contact" href="https://github.com/kinda-dev" target="_blank">Fabio</a>
+      //                 </div>
+      //               </div>
+      //           </div>
+      //       </div>
+ 
+      // </div>
+      <div className='login-outer-wrap'>
+        <Navbar />
+        <div className='login-form-wrap'>
+          <h2 className="sign-in-letters">Sign In</h2>
+          <form onSubmit={this.handleSubmit} className="login-form-box">
+            <div className="login-input-little-box">
+              <input className="login-input-email" placeholder="Email" type="text" value={this.state.email} onChange={this.handleField('email')} />
+              <div className="errors">{this.props.errors.email}</div>
+            </div>
+            <div className="login-input-little-box">
+              <input className="login-input-password" placeholder="Password" type="password" value={this.state.password} onChange={this.handleField('password')}/>
+              <div className="errors">{this.props.errors.password}</div>
+            </div>
+            <div className='login-button-container'>
+              <div className='login-buttons-wrapper'>
+                <button className="login-button" >Sign In</button>
+              </div>
+              <div className='login-buttons-wrapper'>
+                <button onClick={this.loginDemo} className="demo-user-button">Demo User</button>
+              </div>
+            </div>
+          </form>
+          <div className="sign-up-link-login-form">
+            <div className="sign-up-new">New to HaulAway? <Link className="sign-up-link" to="/signup">Sign up now</Link>.</div>
+          </div>
+        </div>    
+        <div className="splash-footer">
+          <div className="splash-footer-wrapper">
+                    {/* <div className="thank you-wrap"> */}
+                    <div className="thank-you">Thank you for your visit</div>
+                    {/* </div> */}
+              <div className="splash-footer-info">
+                <div className="engineerd-by">Engineered with love by:</div>
+                  <div className="info-us">
+                    <a className="contact" href="https://github.com/shinara03" target="_blank">Lena</a>
+                    <a className="contact" href="https://github.com/andmitriy93" target="_blank">Dmitrii</a>
+                    <a className="contact" href="https://github.com/jsadsad" target="_blank">Josh</a>
+                    <a className="contact" href="https://github.com/kinda-dev" target="_blank">Fabio</a>
+                  </div>
                 </div>
             </div>
- 
+        </div>
       </div>
     )
   }
