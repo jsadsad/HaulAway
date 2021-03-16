4e3af717b0f36a6253875dc23f8a28da85ccf300
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-16 22:23:52 +0600

    Reconstruct footer component

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -10,19 +10,26 @@ const Footer = () => {
         {/* </div> */}
         <div className="splash-footer-info">
           <div className="engineerd-by">Engineered with love by:</div>
-          <div className="info-us">
+
+          <div className="info-us-Lena">
             <a className="contact" href="https://github.com/shinara03" target="_blank"><i class='fa fa-github'>Lena</i></a>
             <a className="contact" href="https://github.com/andmitriy93" target="_blank"><i class='fa fa-github'>Dmitrii</i></a>
-            <a className="contact" href="https://github.com/jsadsad" target="_blank"><i class='fa fa-github'>Josh</i></a>
+            <a className="contact" href="" target="_blank"><i class='fa fa-angellist'>Lena</i></a>
             <a className="contact" href="https://github.com/kinda-dev" target="_blank"><i class='fa fa-github'>Fabio</i></a>
           </div>
-          <div className="info-us">
-            <a className="contact" href="" target="_blank"><i class='fa fa-angellist'>Lena</i></a>
+          <div className="info-us-Josh">
+            <a className="contact" href="https://github.com/jsadsad" target="_blank"><i class='fa fa-github'>Josh</i></a>
             <a className="contact" href="" target="_blank"><i class='fa fa-angellist'>Dmitrii</i></a>
             <a className="contact" href="" target="_blank"><i class='fa fa-angellist'>Josh</i></a>
             <a className="contact" href="" target="_blank"><i class='fa fa-angellist'>Fabio</i></a>
           </div>
-          <div className="info-us">
+          <div className="info-us-Fabio">
+            <a className="contact" href="" target="_blank"><i class='fa fa-linkedin'>Lena</i></a>
+            <a className="contact" href="" target="_blank"><i class='fa fa-linkedin'>Dmitrii</i></a>
+            <a className="contact" href="" target="_blank"><i class='fa fa-linkedin'>Josh</i></a>
+            <a className="contact" href="" target="_blank"><i class='fa fa-linkedin'>Fabio</i></a>
+          </div>
+          <div className="info-us-Dmitrii">
             <a className="contact" href="" target="_blank"><i class='fa fa-linkedin'>Lena</i></a>
             <a className="contact" href="" target="_blank"><i class='fa fa-linkedin'>Dmitrii</i></a>
             <a className="contact" href="" target="_blank"><i class='fa fa-linkedin'>Josh</i></a>
