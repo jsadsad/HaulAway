9aab80fb474841df8a6d423ed8d39c9930734ada
kinda-dev <maybe.fabio@gmail.com>
2021-03-16 20:03:34 -0700

    resolve conflict

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,25 +1,32 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './navbar.css'
 
 class Navbar extends React.Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            showMenu: false,
+        }
 
-    this.state = {
-      showMenu: false,
+        this.showMenu = this.showMenu.bind(this);
+        this.logoutUser = this.logoutUser.bind(this);
+        this.getNavbar = this.getNavbar.bind(this);
     }
 
-    this.showMenu = this.showMenu.bind(this)
-    this.logoutUser = this.logoutUser.bind(this)
-    this.getNavbar = this.getNavbar.bind(this)
-  }
+    logoutUser(e) {
+        e.preventDefault();
+        this.props.logout();
+    }
 
-  logoutUser(e) {
-    e.preventDefault()
-    this.props.logout()
-  }
+    showMenu(e) {
+        e.preventDefault();
+        this.setState({
+            showMenu: !this.state.showMenu,
+        })
+    }
 
     getNavbar() {
         console.log(this.state.showMenu)
@@ -35,29 +42,26 @@ class Navbar extends React.Component {
                                 <i className="fas fa-bars"></i>
                             </div> */}
 
-              {/* {this.props.currentUser.firstName} <i className="fas fa-cog"></i> */}
-              <i onClick={this.showMenu} className="fas fa-bars"></i>
+                            {/* {this.props.currentUser.firstName} <i className="fas fa-cog"></i> */}
+                            <i onClick={this.showMenu} className="fas fa-bars"></i>
 
-              {this.state.showMenu ? (
-                <div className="navbar-menu">
-                  <Link
-                    className="navbar-link-user-profile"
-                    to={`/users/${this.props.currentUser.id}`}
-                  >
-                    <div className="navbar-menu-user">
-                      <i className="fas fa-user"></i> Profile
+                            {
+                                this.state.showMenu
+                                    ? (
+                                        <div className="navbar-menu">
+                                            <Link className="navbar-link-user-profile" to={`/users/${this.props.currentUser.id}`}><div className="navbar-menu-user"><i className="fas fa-user"></i> Profile</div></Link>
+                                            <Link className="navbar-link-jobs-index" to={"/jobs"}><div className="navbar-menu-jobs"><i className="fas fa-shuttle-van"></i> All Jobs</div></Link>
+                                            <Link className="navbar-link-jobs-new" to="/jobs/new"><div className="navbar-menu-jobs-new" ><i class='fa fa-handshake-o'></i> Request a Job</div></Link>
+                                            <div className="logout" onClick={this.logoutUser}><i className="fas fa-times-circle"></i> Log Out</div>
+                                        </div>
+                                    )
+                                    : (
+                                        null
+                                    )
+                            }
+                        </div>
+
                     </div>
-                  </Link>
-                  <Link className="navbar-link-jobs-index" to={'/jobs'}>
-                    <div className="navbar-menu-jobs">
-                      <i className="fas fa-shuttle-van"></i> All Jobs
-                    </div>
-                  </Link>
-                  <Link className="navbar-link-jobs-new" to="/jobs/new">
-                    <div className="navbar-menu-jobs-new">
-                      <i class="fa fa-handshake-o"></i> Request a Job
-                    </div>
-<<<<<<< HEAD
                 </div>
             );
         } else {
@@ -75,45 +79,14 @@ class Navbar extends React.Component {
   }  
 
 
-
+    
     render() {
         return (
             <>
                 { this.getNavbar() }
             </>
         )
-=======
-                  </Link>
-                  <div className="logout" onClick={this.logoutUser}>
-                    <i className="fas fa-times-circle"></i> Log Out
-                  </div>
-                </div>
-              ) : null}
-            </div>
-          </div>
-        </header>
-      )
-    } else {
-      return (
-        <header className="navbar">
-          <div className="navbar-wrapper">
-            <div className="navbar-left-wrap">
-              <div className="navbar-logo-wrap">
-                <Link className="logo-link" to={'/'}>
-                  HaulAway
-                </Link>
-              </div>
-            </div>
-          </div>
-        </header>
-      )
->>>>>>> main
     }
-  }
-
-  render() {
-    return <>{this.getNavbar()}</>
-  }
 }
 
-export default Navbar
+export default Navbar;
