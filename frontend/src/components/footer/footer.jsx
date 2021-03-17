d144b3db56b854b9202a46e655da9ee5461b0161
Dmitrii An <an.dmitriy93@gmail.com>
2021-03-17 22:14:51 +0600

    Add links for angellist and linkedin

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -11,23 +11,23 @@ const Footer = () => {
           Lena
           <div className="info-us-contacts">
             <a className="contact" href="https://github.com/shinara03" target="_blank"><i className='fa fa-github'></i></a>
-            <a className="contact" href="" target="_blank"><i className='fa fa-linkedin'></i></a>
-            <a className="contact" href="" target="_blank"><i className='fa fa-angellist'></i></a>
+            <a className="contact" href="https://www.linkedin.com/in/hyeyoung-shin-442228209/" target="_blank"><i className='fa fa-linkedin'></i></a>
+            <a className="contact" href="https://angel.co/u/hyeyoung-lena-shin" target="_blank"><i className='fa fa-angellist'></i></a>
           </div>
         </div>
         <div className="info-us">
           Josh
           <div className="info-us-contacts">
             <a className="contact" href="https://github.com/jsadsad" target="_blank"><i className='fa fa-github'></i></a>
-            <a className="contact" href="" target="_blank"><i className='fa fa-linkedin'></i></a>
-            <a className="contact" href="" target="_blank"><i className='fa fa-angellist'></i></a>
+            <a className="contact" href="https://www.linkedin.com/in/joshsad2/" target="_blank"><i className='fa fa-linkedin'></i></a>
+            <a className="contact" href="https://angel.co/u/jsadsad" target="_blank"><i className='fa fa-angellist'></i></a>
           </div>
         </div>
         <div className="info-us">
           Fabio
           <div className="info-us-contacts">
             <a className="contact" href="https://github.com/kinda-dev" target="_blank"><i className='fa fa-github'></i></a>
-            <a className="contact" href="" target="_blank"><i className='fa fa-linkedin'></i></a>
+            <a className="contact" href="https://www.linkedin.com/in/fabio-raffaele-bortone/" target="_blank"><i className='fa fa-linkedin'></i></a>
             <a className="contact" href="" target="_blank"><i className='fa fa-angellist'></i></a>
           </div>
         </div>
@@ -35,8 +35,8 @@ const Footer = () => {
           Dmitrii
           <div className="info-us-contacts">
             <a className="contact" href="https://github.com/andmitriy93" target="_blank"><i className='fa fa-github'></i></a>
-            <a className="contact" href="" target="_blank"><i className='fa fa-linkedin'></i></a>
-            <a className="contact" href="" target="_blank"><i className='fa fa-angellist'></i></a>
+            <a className="contact" href="https://www.linkedin.com/in/dmitriy-an/" target="_blank"><i className='fa fa-linkedin'></i></a>
+            <a className="contact" href="https://angel.co/u/dmitrii-an" target="_blank"><i className='fa fa-angellist'></i></a>
           </div>
         </div>
       </div>
