3d396ea1dbec5fd750855bd229fc8ac68f3a612b
Joshua C. Sadsad <joshsad2@gmail.com>
2021-03-18 14:46:18 -0700

    map current location working

--- frontend/src/components/job/job_edit.jsx
+++ frontend/src/components/job/job_edit.jsx
@@ -3,6 +3,7 @@ import Navbar from '../navbar/navbar_container'
 import Autocomplete from 'react-google-autocomplete'
 import { uploadPhotos } from '../../util/photo_api_util'
 import { getDistance, convertDistance } from 'geolib'
+import Geocode from 'react-geocode'
 import './job_edit.css'
 
 import { GoogleApiWrapper, Map, Marker, Circle } from 'google-maps-react'
@@ -12,6 +13,8 @@ class JobEdit extends React.Component {
     super(props)
 
     this.state = {
+      pickup: '',
+      destination: '',
       mapPosition: {
         lat: 36.778259,
         lng: -119.417931,
@@ -37,6 +40,7 @@ class JobEdit extends React.Component {
     this.onPickupSelected = this.onPickupSelected.bind(this)
     this.onDestinationSelected = this.onDestinationSelected.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
+    this.distanceRender = this.distanceRender.bind(this)
   }
 
   componentWillUnmount() {
@@ -56,6 +60,23 @@ class JobEdit extends React.Component {
         jobEndDate: this.props.job ? this.props.job.jobEndDate : '',
         jobType: this.props.job ? this.props.job.jobType : '',
         pictures: this.props.job ? this.props.job.pictures : [],
+      })
+      this.getCoords()
+    })
+  }
+
+  getCoords() {
+    Geocode.fromAddress(this.props.job.destination).then((res) => {
+      const { lat, lng } = res.results[0].geometry.location
+      this.setState({
+        markerPosition: {
+          lat: lat,
+          lng: lng,
+        },
+        mapPosition: {
+          lat: lat,
+          lng: lng,
+        },
       })
     })
   }
@@ -77,6 +98,8 @@ class JobEdit extends React.Component {
           _id: this.props.jobId,
           description: this.state.description,
           pickup: this.state.pickup,
+          distance: this.distanceRender().toFixed(2),
+          price: this.priceRender().toFixed(2),
           destination: this.state.destination,
           jobDifficulty: this.state.jobDifficulty,
           jobType: this.state.jobType,
@@ -93,6 +116,8 @@ class JobEdit extends React.Component {
         _id: this.props.jobId,
         description: this.state.description,
         pickup: this.state.pickup,
+        distance: this.distanceRender().toFixed(2),
+        price: this.priceRender().toFixed(2),
         destination: this.state.destination,
         jobDifficulty: this.state.jobDifficulty,
         jobType: this.state.jobType,
@@ -125,10 +150,16 @@ class JobEdit extends React.Component {
   }
 
   onPickupSelected(place) {
-    const address = place.formatted_address
+    const address = place.formatted_address,
+      latValue = place.geometry.location.lat(),
+      lngValue = place.geometry.location.lng()
 
     this.setState({
       pickup: address ? address : '',
+      pickupCoords: {
+        lat: latValue,
+        lng: lngValue,
+      },
     })
   }
 
@@ -138,6 +169,10 @@ class JobEdit extends React.Component {
       lngValue = place.geometry.location.lng()
     this.setState({
       destination: address ? address : '',
+      destinationCoords: {
+        lat: latValue,
+        lng: lngValue,
+      },
       markerPosition: {
         lat: latValue,
         lng: lngValue,
@@ -150,31 +185,27 @@ class JobEdit extends React.Component {
   }
 
   distanceRender() {
-    if (this.state.destination) {
-      if (
-        this.state.destination === '' ||
-        this.state.pickup === '' ||
-        !this.state.destination.includes('USA')
-      ) {
-        return 0
-      } else {
-        return convertDistance(
-          getDistance(
-            {
-              latitude: this.state.pickupCoords.lat,
-              longitude: this.state.pickupCoords.lng,
-            },
-            {
-              latitude: this.state.destinationCoords.lat,
-              longitude: this.state.destinationCoords.lng,
-            },
-            0.01
-          ),
-          'mi'
-        )
-      }
-    } else {
+    if (
+      this.state.destination === this.props.job.destination ||
+      this.state.pickup === this.props.job.pickup ||
+      !this.state.destination.includes('USA')
+    ) {
       return 0
+    } else {
+      return convertDistance(
+        getDistance(
+          {
+            latitude: this.state.pickupCoords.lat,
+            longitude: this.state.pickupCoords.lng,
+          },
+          {
+            latitude: this.state.destinationCoords.lat,
+            longitude: this.state.destinationCoords.lng,
+          },
+          0.01
+        ),
+        'mi'
+      )
     }
   }
 
@@ -239,8 +270,7 @@ class JobEdit extends React.Component {
                     style={{ width: '25%' }}
                     types={['address']}
                     onChange={this.handleField('pickup')}
-                    componentRestrictions={{ country: 'us' }}
-                    defaultValue={job.pickup}
+                    // defaultValue={job.pickup}
                     placeholder={job.pickup}
                   />
                   {this.props.errors.pickup}
@@ -253,21 +283,21 @@ class JobEdit extends React.Component {
                     types={['address']}
                     componentRestrictions={{ country: 'us' }}
                     onChange={this.handleField('destination')}
-                    defaultValue={job.destination}
+                    // defaultValue={job.destination}
                     placeholder={job.destination}
                   />
                   {this.props.errors.destination}
                 </div>
                 <div className="form-distance-container">
                   <label className="form-distance-text">
-                    Previous Distance: {this.state.distance}
+                    Previous Distance: {job.distance}
                     <br />
                     New Distance: {this.distanceRender().toFixed(2)} miles
                   </label>
                 </div>
                 <div className="form-distance-container">
                   <label className="form-distance-text">
-                    Previous Price: {this.state.price}
+                    Previous Price: ${job.price}
                     <br />
                     New Price: $ {this.priceRender().toFixed(2)}
                   </label>
@@ -364,6 +394,7 @@ class JobEdit extends React.Component {
   }
 }
 
+Geocode.setApiKey('AIzaSyBQf1ahKg7gbuVHd9daHMMvm0zfPEpnBd8')
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyBQf1ahKg7gbuVHd9daHMMvm0zfPEpnBd8',
 })(JobEdit)
