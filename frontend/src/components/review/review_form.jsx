51d8d96cd767c62302582a53e96142f2bfdec24a
kinda-dev <maybe.fabio@gmail.com>
2021-03-18 19:35:38 -0700

    style review form

--- frontend/src/components/review/review_form.jsx
+++ frontend/src/components/review/review_form.jsx
@@ -80,9 +80,9 @@ class ReviewForm extends Component {
             <h2 className="review-form-header">Write a Review</h2>
             <form className="review-form-box"
                 onSubmit={this.handleSubmit} >
-              <div className="review-form-input-above-little-box">
+              <div className="review-form-inner-box">
                 <input className="review-title-input" type="text" 
-                      placeholder='Title'
+                      placeholder='Please insert a title'
                       onChange={this.handleField('title')}/>
 
                 <select className='review-form-select'
@@ -100,11 +100,9 @@ class ReviewForm extends Component {
 
 
               </div>
-              <div className="review-form-input-below-little-box">
                 <textarea className="review-body-input" 
-                          placeholder='Body'
+                          placeholder='Describe your experience to help other users'
                           onChange={this.handleField('body')}/>
-              </div>
               <div>
                 <button className="job-form-btn">Post Review</button>
               </div>
