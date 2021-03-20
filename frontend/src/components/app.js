3e7f47be3bb52798e3f655fff47fcbfa20ee45f2
kinda-dev <maybe.fabio@gmail.com>
2021-03-19 19:32:05 -0700

    import route app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AuthRoute, ProtectedRoute } from '../util/route_util'
-import { Switch } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import Modal from '../components/modal/modal'
 import SplashPage from './splash/splash_page'
 import HomepageContainer from './homepage/homepage_container'
