6b23e840924556d842d40e55f9388f4d231e6dd8
Joshua C. Sadsad <joshsad2@gmail.com>
2021-03-21 11:32:05 -0700

    user name styling on navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,92 +1,101 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 import './navbar.css'
 
 class Navbar extends React.Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            showMenu: false,
-        }
+  constructor(props) {
+    super(props)
 
-        this.showMenu = this.showMenu.bind(this);
-        this.logoutUser = this.logoutUser.bind(this);
-        this.getNavbar = this.getNavbar.bind(this);
+    this.state = {
+      showMenu: false,
     }
 
-    logoutUser(e) {
-        e.preventDefault();
-        this.props.logout();
-    }
+    this.showMenu = this.showMenu.bind(this)
+    this.logoutUser = this.logoutUser.bind(this)
+    this.getNavbar = this.getNavbar.bind(this)
+  }
 
-    showMenu(e) {
-        e.preventDefault();
-        this.setState({
-            showMenu: !this.state.showMenu,
-        })
-    }
+  logoutUser(e) {
+    e.preventDefault()
+    this.props.logout()
+  }
 
-    getNavbar() {
-        if (this.props.loggedIn) {
-            return (
-                <div className="navbar">
-                    <div className="navbar-wrapper">
-                        <div className="navbar-left-wrap">
-                            <div className="navbar-logo-wrap"><Link className="logo-link" to={'/'}>HaulAway</Link></div>
-                        </div>
-                        <div className="navbar-right-wrap">
-                            <Link className="navbar-about-us-link" to="/about">About Us</Link>
-                            {/* <div className="navbar-menu-logo-wrap">
-                                <i className="fas fa-bars"></i>
-                            </div> */}
+  showMenu(e) {
+    e.preventDefault()
+    this.setState({
+      showMenu: !this.state.showMenu,
+    })
+  }
 
-                            {/* {this.props.currentUser.firstName} <i className="fas fa-cog"></i> */}
-                            <i onClick={this.showMenu} className="fas fa-bars"></i>
-
-                            {
-                                this.state.showMenu
-                                    ? (
-                                        <div className="navbar-menu">
-                                            <Link className="navbar-link-user-profile" to={`/users/${this.props.currentUser.id}`}><div className="navbar-menu-user"><i className="fas fa-user"></i>{this.props.currentUser.firstName}</div></Link>
-                                            <Link className="navbar-link-jobs-index" to={"/jobs"}><div className="navbar-menu-jobs"><i className="fas fa-shuttle-van"></i> All Jobs</div></Link>
-                                            <Link className="navbar-link-jobs-new" to="/jobs/new"><div className="navbar-menu-jobs-new" ><i className="fa fa-handshake-o"></i> Request a Job</div></Link>
-                                            <div className="logout" onClick={this.logoutUser}><i className="fas fa-times-circle"></i> Log Out</div>
-                                        </div>
-                                    )
-                                    : (
-                                        null
-                                    )
-                            }
-                        </div>
-
-                    </div>
-                </div>
-            );
-        } else {
-          return (
-              <div className="navbar">
-                  <div className="navbar-wrapper">
-                      <div className="navbar-left-wrap">
-                          <div className="navbar-logo-wrap"><Link className="logo-link" to={'/'}>HaulAway</Link></div>
-                      </div>
-                  </div>
+  getNavbar() {
+    if (this.props.loggedIn) {
+      return (
+        <div className="navbar">
+          <div className="navbar-wrapper">
+            <div className="navbar-left-wrap">
+              <div className="navbar-logo-wrap">
+                <Link className="logo-link" to={'/'}>
+                  HaulAway
+                </Link>
               </div>
-          );
-        }
+            </div>
+            <div className="navbar-right-wrap">
+              <Link className="navbar-about-us-link" to="/about">
+                About Us
+              </Link>
+              <i onClick={this.showMenu} className="fas fa-bars"></i>
 
-  }  
-
-
-    
-    render() {
-        return (
-            <>
-                { this.getNavbar() }
-            </>
-        )
+              {this.state.showMenu ? (
+                <div className="navbar-menu">
+                  <Link
+                    className="navbar-link-user-profile"
+                    to={`/users/${this.props.currentUser.id}`}
+                  >
+                    <div className="navbar-menu-user">
+                      <i className="fas fa-user"></i>{' '}
+                      {this.props.currentUser.firstName}
+                    </div>
+                  </Link>
+                  <Link className="navbar-link-jobs-index" to={'/jobs'}>
+                    <div className="navbar-menu-jobs">
+                      <i className="fas fa-shuttle-van"></i> All Jobs
+                    </div>
+                  </Link>
+                  <Link className="navbar-link-jobs-new" to="/jobs/new">
+                    <div className="navbar-menu-jobs-new">
+                      <i className="fa fa-handshake-o"></i> Request a Job
+                    </div>
+                  </Link>
+                  <div className="logout" onClick={this.logoutUser}>
+                    <i className="fas fa-times-circle"></i> Log Out
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div className="navbar">
+          <div className="navbar-wrapper">
+            <div className="navbar-left-wrap">
+              <div className="navbar-logo-wrap">
+                <Link className="logo-link" to={'/'}>
+                  HaulAway
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
     }
+  }
+
+  render() {
+    return <>{this.getNavbar()}</>
+  }
 }
 
-export default Navbar;
+export default Navbar
